d19af1131c8f38890f728447a9c4803a50bdf68e
Farah Mahmoud <45809462+fr7m7moud70@users.noreply.github.com>
2020-06-02 14:25:02 +0200

    adding register general component

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Login from "./container/LoginPage/login";
+import RegisterGeneralInfo from "./container/RegisterPage/registerGeneralInfo";
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Login />
+        {/* <Login /> */}
+        <RegisterGeneralInfo />
       </React.Fragment>
     );
   }
