cafbf858fdddbf5ef49c18f260f4060ed3a3a1b8
MennaAhmed95 <mennaallahahmed63@yahoo.com>
2020-06-02 12:36:31 +0200

    edit

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import "../src/sass/main.scss";
+
 import Login from "./container/LoginPage/login";
 import Footer from "./components/layouts/footer";
+import Header from "./components/layouts/header";
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         {/* <Login /> */}
+        <Header />
+        <br /> <br /> <br />
         <Footer />
       </React.Fragment>
     );
