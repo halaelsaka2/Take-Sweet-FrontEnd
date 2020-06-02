cb97a3945d929c98c7123b2c552c0fd1cb240310
Farah Mahmoud <45809462+fr7m7moud70@users.noreply.github.com>
2020-06-02 17:22:00 +0200

    add register acceptance component

--- src/app.jsx
+++ src/app.jsx
@@ -2,29 +2,29 @@ import React, { Component } from "react";
 
 import Login from "./container/LoginPage/login";
 
-        import ProductCardModal from "./container/ProductCardModalPage/ProductCardModal";
+import ProductCardModal from "./container/ProductCardModalPage/ProductCardModal";
 import RegisterGeneralInfo from "./container/RegisterPage/registerGeneralInfo";
 import Footer from "./components/layouts/footer";
 import Header from "./components/layouts/header";
 import Textarea from "./components/Textarea/textarea";
+import RegisterAcceptanceInfo from "./container/RegisterPage/registerAcceptanceInfo";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         {/* <Login /> */}
-           <ProductCardModal />
-
-        <RegisterGeneralInfo />
-        <Header check="t" />
-        <br /> <br /> <br />
-        <Textarea
+        {/* <ProductCardModal /> */}
+        <RegisterAcceptanceInfo />
+        {/* <RegisterGeneralInfo /> */}
+        {/* <Header check="t" /> */}
+        {/* <br /> <br /> <br /> */}
+        {/* <Textarea
           classname="inputReview reviewCard__reviews"
           columsNo="100"
           placeholder="your message"
-        ></Textarea>
-        <Footer />
-
+        ></Textarea> */}
+        {/* <Footer /> */}
       </React.Fragment>
     );
   }
