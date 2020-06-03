9059c2448acfe62172cc93e29799df4789525a54
EmanSayed94 <emanelsayed.33@yahoo.com>
2020-06-03 17:02:49 +0200

    product-card-item&product-card-listing&product-card-container&product-card-modal

--- src/app.jsx
+++ src/app.jsx
@@ -2,20 +2,22 @@ import React, { Component } from "react";
 
 import Login from "./container/LoginPage/login";
 
-import ProductCardModal from "./container/ProductCardModalPage/ProductCardModal";
+import ProductCardModal from "./container/ProductCardModalPage/productCardModal";
 import RegisterGeneralInfo from "./container/RegisterPage/registerGeneralInfo";
 import Footer from "./components/layouts/footer";
 import Header from "./components/layouts/header";
 import Textarea from "./components/Textarea/textarea";
 import RegisterAcceptanceInfo from "./container/RegisterPage/registerAcceptanceInfo";
+import ProductCard from "./components/productCard/productCard";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         {/* <Login /> */}
-        {/* <ProductCardModal /> */}
-        <RegisterAcceptanceInfo />
+        <ProductCardModal />
+        {/* <ProductCard cardType="statusCard" className="product-card--large" /> */}
+        {/* <RegisterAcceptanceInfo /> */}
         {/* <RegisterGeneralInfo /> */}
         {/* <Header check="t" /> */}
         {/* <br /> <br /> <br /> */}
