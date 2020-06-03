213bbe24bf2f3c2c38094438e97f91df4f4f8ff6
EmanSayed94 <emanelsayed.33@yahoo.com>
2020-06-03 18:36:24 +0200

    search component

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import Login from "./container/LoginPage/login";
 
-import ProductCardModal from "./container/ProductCardModalPage/productCardModal";
+import ProductCardModal from "./container/ProductCardModalPage/ProductCardModal";
 import RegisterGeneralInfo from "./container/RegisterPage/registerGeneralInfo";
 import Footer from "./components/layouts/footer";
 import Header from "./components/layouts/header";
@@ -15,15 +15,16 @@ import ProductItem from "../src/components/ProductItem";
 import Slideshow from "../src/components/SlideShow";
 
 import HistoryStates from "./components/HistoryStates";
+import Search from "./components/Search";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Search />
         {/* <Login /> */}
-
-        <ProductCardModal />
-        {/* <ProductCard cardType="statusCard" className="product-card--large" /> */}
+        {/* <ProductCardModal /> */}
+        {/* <ProductCard cardType="historyCard" className="product-card--large" /> */}
         {/* <ProductCardModal /> */}
         {/* <RegisterAcceptanceInfo /> */}
         {/* <RegisterGeneralInfo /> */}
@@ -46,10 +47,10 @@ class App extends Component {
         deserve all waiting time.It is the most crowded place on the street,I
         totally recommend this bakery, their desserts are delicious."
         /> */}
-        <ReviewCard />
+        {/* <ReviewCard /> */}
         {/* history tab component */}
         {/* <HistoryStates tabs={["all", "waiting"]} isActive={true} index={1} /> */}
-        <Slideshow></Slideshow>
+        {/* <Slideshow></Slideshow> */}
       </React.Fragment>
     );
   }
