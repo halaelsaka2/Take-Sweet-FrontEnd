b461a3438e18e914d77ba1ebe0fda8d4c28bc548
salmawagdy96 <59780537+salmawagdy96@users.noreply.github.com>
2020-06-04 02:34:37 +0200

    add bestsellers component

---
+++ src/components/BestSellersItem/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Bestsellersitem = props => {
+  const product = this.props.product;
+  return (
+    <div class="item-medium">
+      <div
+        class="item-medium__image image"
+        style={{
+          backgroundImage: url(product.src),
+          "background-size": "cover",
+          "background-repeat": "no-repeat",
+          "background-position": "center"
+        }}
+      ></div>
+      <a href="#">
+        <h4>${product.name}</h4>
+      </a>
+      <div class="item-medium__crud-actions">
+        <a href="#">
+          <i class="fas fa-edit"></i>
+        </a>
+        <a href="#">
+          <i class="fas fa-trash-alt"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Bestsellersitem;
