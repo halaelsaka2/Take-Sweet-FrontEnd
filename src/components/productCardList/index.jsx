e57b37ecd7d84d0e651b6ad1dfd67e3a8ccc1d95
EmanSayed94 <emanelsayed.33@yahoo.com>
2020-06-05 01:09:13 +0200

    use &&  use Button component instesd of html buton tag    adding handle delete,handle reorderuse

--- src/components/productCardList/index.jsx
+++ src/components/productCardList/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ProductCard from "../productCard/productCard";
 
-const ProductCardList = ({ productCards }) =>
+const ProductCardList = ({ productCards, handleDelete }) =>
   productCards.map((product) => (
     <ProductCard
       productName={product.name}
       productAmount={product.amount}
       productImg={product.image}
       totalPrice={product.totalPrice}
+      handleDelete={handleDelete}
       cardType="modalCard"
     />
   ));
