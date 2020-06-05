d450c1d487419f1ee632198b8b374d69a4d0d404
MennaAhmed95 <mennaallahahmed63@yahoo.com>
2020-06-05 11:24:16 +0200

    Card Order Component

--- src/App/app.jsx
+++ src/App/app.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 
-import Footer from "./../components/Layouts/Footer";
-
 class App extends Component {
   render() {
     return <React.Fragment></React.Fragment>;
--- src/components/ٍShoppingOrderContainer/index.jsx
+++ src/components/ٍShoppingOrderContainer/index.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-import ShoppingOrderList from "./ShoppingOrderList";
 import ShoppingCart from "../ShoppingCart";
+import ShoppingOrderList from "./Partails/shopingOrderList";
 
-const ShoppingOrderContainer = (props) => (
-  <React.Fragment>
-    <ShoppingCart></ShoppingCart>
-    <div className="shoppingCart">
-      <div className="shoppingCart__title">
-        <h4>Shopping Bag</h4>
-        <i className="fas fa-times"></i>
+{
+  /*<ShoppingOrderContainer
+shoppingOrderList={this.state.shoppingOrderList}
+/>*/
+}
+const ShoppingOrderContainer = ({ shoppingOrderList }) => {
+  return (
+    <React.Fragment>
+      <ShoppingCart />
+      <div className="shoppingCart">
+        <div className="shoppingCart__title">
+          <h4>Shopping Bag</h4>
+          <i className="fas fa-times"></i>
+        </div>
+        {shoppingOrderList.length === 0 && (
+          <h5>you have no items in shopping card...</h5>
+        )}
+        <ShoppingOrderList shoppingOrderList={shoppingOrderList} />
       </div>
-      <h5>you have no items in shopping card...</h5>
-      <ShoppingOrderList
-        shoppingOrderList={props.ShoppingOrderList}
-      ></ShoppingOrderList>
-    </div>
-  </React.Fragment>
-);
+    </React.Fragment>
+  );
+};
 
 export default ShoppingOrderContainer;
