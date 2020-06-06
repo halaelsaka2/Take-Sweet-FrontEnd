db973cb0283934add670c7538153f52218840ed1
EmanSayed94 <emanelsayed.33@yahoo.com>
2020-06-07 00:53:03 +0200

    fix import errors

--- src/components/historyFromUserSection/index.jsx
+++ src/components/historyFromUserSection/index.jsx
@@ -4,10 +4,10 @@ import Search from "../Search";
 
 import { Container } from "reactstrap";
 import HistoryStates from "../HistoryStates";
-import ProductCardList from "../productCardList";
+import ProductCardList from "../ProductCardList";
 import Footer from "../Layouts/Footer";
 import OrderDate from "./../OrderDate";
-import Pagination from "./../pagination";
+import Pagination from "./../Pagination";
 import CompanyLogo from "./../CompanyLogo";
 
 const HistoryFromUserSection = ({
