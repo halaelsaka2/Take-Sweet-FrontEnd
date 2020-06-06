e64f29a3e0145200910717b019368e27a32074d8
ahmed <ahmedazizsq@gmail.com>
2020-06-06 21:06:16 +0200

    adding pratials folder and remove BranchList and BranchItem folders

--- src/components/BranchContainer/index.jsx
+++ src/components/BranchContainer/index.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import BranchList from "./BranchList";
 import ProfileLabel from "../ProfileLabel";
 
-const BranchContiner = ({ branchList }) => (
+const BranchContiner = ({ branchList, isEditBranch }) => (
   <React.Fragment>
-    <ProfileLabel className="profileLabel" content="Branches"/>
+    <ProfileLabel className="profileLabel" content="Branches" />
     <div className="branchContainer">
       <div className="branchContainer__content">
-        <div className="branchContainer__edit">
-          <div className="branchContainer__edit__icon" id="editBranch">
-            <i className="fas fa-pencil-alt"></i>
+        {isEditBranch && (
+          <div className="branchContainer__edit">
+            <div className="branchContainer__edit__icon" id="editBranch">
+              <i className="fas fa-pencil-alt"></i>
+            </div>
           </div>
-        </div>
+        )}
+
         <div class="branchContainer__branches">
           <BranchList branchList={branchList}></BranchList>;
         </div>
--- src/components/BranchContainer/partials/BranchItem.jsx
+++ src/components/BranchContainer/partials/BranchItem.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-const BranchItem = ({ cityName, address, phoneNumber }) => (
-  <div className="branchContainer__branches__location">
+const BranchItem = ({
+  cityName,
+  address,
+  phoneNumber,
+  cancelIcon,
+  itemStyle,
+}) => (
+  <div className={`"branchContainer__branches__location"${cancelIcon}`}>
     <span className="branchContainer__branches__location__header">
       {cityName}
     </span>
+    {cancelIcon && <i class="fas fa-times cancel-branch"></i>}
     <span className="branchContainer__branches__location__address">
       {address}
     </span>
