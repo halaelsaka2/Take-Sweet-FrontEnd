c61a351999a1967163357af13c9329a9b7d97539
ahmed <ahmedazizsq@gmail.com>
2020-06-06 16:32:20 +0200

    adding productspage component

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 
-const ProductItem = (props) => {
+const ProductItem = ({ src, amount, pagetype, name }) => {
   return (
     <div calssName="itemm">
-      <div
-        calssName="itemm__image"
-        style={{backgroundImage: `url(${props.src})`}}
-      >
+      <div calssName="itemm__image" style={{ backgroundImage: `url(${src})` }}>
         <div calssName="itemm__data">
-          <button calssName="itemm__btn">Order Now</button>
+          {pagetype === "buyerPage" ? (
+            <button calssName="itemm__btn">Order Now</button>
+          ) : (
+            {}
+          )}
         </div>
       </div>
-      <h4>{props.Name}</h4>
+      <h4>{name}</h4>
       <div calssName="itemm__data__amount">
-        <i calssName="fas fa-minus itemm__data__amount__controls"></i>
-        <div calssName="itemm__data__amount__number">{props.amount}</div>
-        <i calssName="fas fa-plus itemm__data__amount__controls"></i>
+        {pagetype === "buyerPage" ? (
+          <div calssName="itemm__data__amount">
+            <i calssName="fas fa-minus itemm__data__amount__controls"></i>
+            <div calssName="itemm__data__amount__number">{amount}</div>
+            <i calssName="fas fa-plus itemm__data__amount__controls"></i>
+          </div>
+        ) : (
+          <div class="item-medium__crud-actions">
+            <div class="flex-row">
+              <div>
+                <span class="lable"> 10 pieces</span>
+              </div>
+            </div>
+            <a href="#">
+              <i class="fas fa-edit"></i>
+            </a>
+            <a href="#">
+              <i class="fas fa-trash-alt"></i>
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
