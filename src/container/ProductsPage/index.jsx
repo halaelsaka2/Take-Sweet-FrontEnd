8f4adf294315af503ecb79a3dea0cd86ec3d3385
ahmed <ahmedazizsq@gmail.com>
2020-06-06 20:03:16 +0200

    modify productspage textarea

--- src/container/ProductsPage/index.jsx
+++ src/container/ProductsPage/index.jsx
@@ -1,41 +1,57 @@
 import React, { Component } from "react";
-import Header from "../../components/layouts/header";
-import Footer from "../../components/layouts/footer";
+import Header from "../../components/Layouts/Header";
+import Footer from "../../components/Layouts/Footer";
 import Search from "../../components/Search";
-import ProductItem from "../../components/";
+import ProductItem from "../../components/ProductItem";
+import DropDown from "../../components/Dropdown";
+import DescriptionSection from "../../components/DescriptionScetion";
 
-export class ProductsPage extends Component {
+export default class ProductsPage extends Component {
   state = {
     products: [],
+    sortbyList: [],
+    categoryList: [],
   };
 
   render() {
     return (
       <React.Fragment>
         <Header></Header>
-        {this.props.pageType === "buyerPage" ? (
-          <div class="text-container">
-            <h3 class="text-container-h3">Our Products</h3>
-            <br />
-            Whether you are looking for the perfect cake for a celebration,
-            browsing breakfast ideas on searching for catering options for large
-            meetings or celebrations; our food has something for everyone.
+        <div class="project-container project-container--h">
+          {this.props.pageType === "buyerPage" && (
+            <DescriptionSection
+              titleClassName={"text-container-h3"}
+              title={"Our Products"}
+              textClassName={"text-container"}
+              text={`Whether you are looking for the perfect cake for a celebration,
+            browsing breakfast ideas on searching for catering options for
+            large meetings or celebrations; our food has something for
+            everyone`}
+            ></DescriptionSection>
+          )}
+          <div class="tabs-containera">
+            <DropDown
+              dropdownListItems={this.state.sortbyList}
+              isOpened={false}
+              dropdownHeader={"sortBy"}
+            ></DropDown>
+            <DropDown
+              dropdownListItems={this.state.categoryList}
+              isOpened={false}
+              dropdownHeader={"Category"}
+            ></DropDown>
+            <Search></Search>
           </div>
-        ) : (
-          {}
-        )}
-        <div class="tabs-containera">
-          <Search></Search>
-        </div>
-        <div class="list-container">
-          {this.state.map((item) => (
-            <ProductItem
-              name={item.name}
-              src={item.src}
-              amount={item.amount}
-              pageType={this.props.pageType}
-            ></ProductItem>
-          ))}
+          <div class="list-container">
+            {this.state.products.map((item) => (
+              <ProductItem
+                name={item.name}
+                src={item.src}
+                amount={item.amount}
+                pageType={this.props.pageType}
+              ></ProductItem>
+            ))}
+          </div>
         </div>
         <Footer></Footer>
       </React.Fragment>
