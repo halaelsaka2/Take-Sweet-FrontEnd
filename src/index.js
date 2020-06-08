94cbfffa60d4f8d3f314e3445979463e04fb2cfb
MennaAhmed95 <mennaallahahmed63@yahoo.com>
2020-06-09 00:51:56 +0200

    remove routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./sass/main.scss";
-import { BrowserRouter } from "react-router-dom";
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
-const app = () => (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
 
-ReactDOM.render(app, document.querySelector("#root"));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
