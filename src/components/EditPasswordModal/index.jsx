741ae0dbd3be36591ddb5a455362ce007b649814
Hala Elsaka <halaelsaka2@gmail.com>
2020-06-08 16:29:19 +0200

    restructure-props-and-add-propTypes-and-remove-return

--- src/components/EditPasswordModal/index.jsx
+++ src/components/EditPasswordModal/index.jsx
@@ -1,57 +1,73 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Input from "../Input";
 import Button from "../Button";
-const EditPasswordModal = ({ onChange ,value,onSave,onCancel}) => {
-  return (
-    <div class="myModal__modalContent__body">
-      <div>
-        <Input
-          onChange={onChange}
-          id="oldPassword"
-          name="oldPassword"
-          type="password"
-          className="input input--s"
-          placeholder=" Old password"
-          value={value}
-        />
-      </div>
-      <div>
-        <Input
-          onChange={onChange}
-          id="newPassword"
-          name="newPassword"
-          type="password"
-          className="input input--s"
-          placeholder="New password"
-          value={value}
-        />
-      </div>
-      <div>
-        <Input
-          onChange={onChange}
-          id="confirmPassword"
-          name="confirmPassword"
-          type="password"
-          className="input input--s"
-          placeholder="Confirm password"
-          value={value}
-        />
-      </div>
-      <div class="lastContent__buttons">
-        <Button
-          className="button--rounded button--rounded--s"
-          name="Save"
-          id="savePassword"
-          onClick={onSave}
-        />
-        <Button
-          className="button--rounded button--rounded--s button--rounded--danger"
-          name="Cancel"
-          id="closepassword"
-          onClick={onCancel}
-        />
-      </div>
+
+const EditPasswordModal = ({
+  onChange,
+  oldPasswordValue,
+  newPasswordValue,
+  confirmPasswordValue,
+  onSave,
+  onCancel,
+}) => (
+  <div class="myModal__modalContent__body">
+    <div>
+      <Input
+        onChange={onChange}
+        id="oldPassword"
+        name="oldPassword"
+        type="password"
+        className="input input--s"
+        placeholder=" Old password"
+        value={oldPasswordValue}
+      />
     </div>
-  );
+    <div>
+      <Input
+        onChange={onChange}
+        id="newPassword"
+        name="newPassword"
+        type="password"
+        className="input input--s"
+        placeholder="New password"
+        value={newPasswordValue}
+      />
+    </div>
+    <div>
+      <Input
+        onChange={onChange}
+        id="confirmPassword"
+        name="confirmPassword"
+        type="password"
+        className="input input--s"
+        placeholder="Confirm password"
+        value={confirmPasswordValue}
+      />
+    </div>
+    <div class="lastContent__buttons">
+      <Button
+        className="button--rounded button--rounded--s"
+        name="Save"
+        id="savePassword"
+        onClick={onSave}
+      />
+      <Button
+        className="button--rounded button--rounded--s button--rounded--danger"
+        name="Cancel"
+        id="closepassword"
+        onClick={onCancel}
+      />
+    </div>
+  </div>
+);
+
+EditPasswordModal.propTypes = {
+  oldPasswordValue: PropTypes.string,
+  newPasswordValue: PropTypes.string,
+  confirmPasswordValue: PropTypes.string,
+  onChange: PropTypes.func,
+  onCancel: PropTypes.func,
+  onSave: PropTypes.func,
 };
 export default EditPasswordModal;
