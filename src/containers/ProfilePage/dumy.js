2f25e8e0ef492406ef10dfa424b841c203379712
Hala Elsaka <halaelsaka2@gmail.com>
2020-06-09 03:32:56 +0200

    add constant and dumy files and add openModal function

---
+++ src/containers/ProfilePage/dumy.js
@@ -0,0 +1,48 @@
+export const dumy ={
+    products: [
+        {
+          id: 1,
+          name: "product",
+          src: "assets/images/Product-1.jpg",
+        },
+        {
+          id: 2,
+          name: "product",
+          src: "assets/images/Product-1.jpg",
+        },
+        {
+          id: 3,
+          name: "product",
+          src: "assets/images/Product-1.jpg",
+        },
+        {
+          id: 4,
+          name: "product",
+          src: "assets/images/Product-1.jpg",
+        },
+      ],
+      userProfile: {
+        email: "halaelsaka2@gmail.com",
+        userName: "Nola Cafe",
+        discription:
+          "blablablablabalablablbalablbalbblalblablablablablablblablablabalb",
+        imageUrl: "",
+      },
+      branchList: [
+        {
+          cityName: "Ismailia",
+          address: "Ismailia, nemra6",
+          phoneNumber: "+012 999 999 999",
+        },
+        {
+          cityName: "Cairo",
+          address: "New Cairo, tseen street",
+          phoneNumber: "+012 999 999 999",
+        },
+        {
+          cityName: "Alexandria",
+          address: "mahatt el raml",
+          phoneNumber: "+012 999 999 999",
+        },
+      ],
+}
