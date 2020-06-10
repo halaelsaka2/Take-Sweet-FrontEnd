2bbf2eb7e1473b274a631cdab709237b42ea5b82
salmawagdy96 <59780537+salmawagdy96@users.noreply.github.com>
2020-06-10 14:57:12 +0200

    fix-best-section-items

--- src/containers/Homepage/index.jsx
+++ src/containers/Homepage/index.jsx
@@ -3,18 +3,15 @@ import Header from "../../components/Layouts/Header";
 import Footer from "../../components/Layouts/Footer";
 import HomePageSection from "../../components/HomePageSection/HomePageSection";
 import constents from "./constents";
-import { container } from "reactstrap";
 
 const Home = () => {
   return (
     <React.Fragment>
       <Header />
-      <container>
-        <HomePageSection
-          AboutUsContext={constents.AboutUsContext}
-          companies={constents.companies}
-        />
-      </container>
+      <HomePageSection
+        AboutUsContext={constents.AboutUsContext}
+        companies={constents.companies}
+      />
       <Footer />
     </React.Fragment>
   );
