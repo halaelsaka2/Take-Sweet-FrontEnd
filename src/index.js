22554c5c43031b736a7b531e3e295102c3009655
Farah Mahmoud <45809462+fr7m7moud70@users.noreply.github.com>
2020-06-10 16:32:11 +0200

    adding redux to the project (not finished yet)

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import App from "./containers/App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./sass/main.scss";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './redux-modules/store';
 
 const app = (
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
-
-ReactDOM.render(app, document.querySelector("#root"));
+ReactDOM.render(<Provider store={store}>{app}</Provider>, document.querySelector("#root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
