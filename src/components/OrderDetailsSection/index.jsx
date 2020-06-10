df41615138ee3e7d03fe0f7b021faa2ed8111671
ahmed <ahmedazizsq@gmail.com>
2020-06-10 20:17:54 +0200

    add className

--- src/components/OrderDetailsSection/index.jsx
+++ src/components/OrderDetailsSection/index.jsx
@@ -7,56 +7,56 @@ import { Table } from "reactstrap";
 const OrderDetaislSection = () => (
   <React.Fragment>
     <Header />
-    <div class="project-container--h">
-      <div class="productLabel">Order Details</div>
+    <div className="project-container--h">
+      <div className="productLabel">Order Details</div>
     </div>
-    <div class="history__line"></div>
-    <div class="details-table">
+    <div className="history__line"></div>
+    <div className="details-table">
       <Table></Table>
     </div>
-    <div class="detailsContainerr">
-      <div class="paymentType-container">
-        <div class="paymentType-container__content">
+    <div className="detailsContainerr">
+      <div className="paymentType-container">
+        <div className="paymentType-container__content">
           <label> Payment Types</label>
-          <div class="status-current status-current--details">
-            <div class="wrapping">
+          <div className="status-current status-current--details">
+            <div className="wrapping">
               <input
                 type="radio"
                 id="cash"
                 name="card-0"
-                class="status-current__circle"
+                className="status-current__circle"
               />
               <label for="cash">Cash</label>
             </div>
-            <div class="wrapping">
+            <div className="wrapping">
               <input
                 type="radio"
                 id="us"
                 name="card-0"
-                class="status-current__circle"
+                className="status-current__circle"
               />
               <label for="creditCard">Credit Card</label>
             </div>
-            <div class="wrapping">
+            <div className="wrapping">
               <input
                 type="radio"
                 id="mobile"
                 name="card-0"
-                class="status-current__circle"
+                className="status-current__circle"
               />
               <label for="mobile">Mobile Payment</label>
             </div>
           </div>
         </div>
       </div>
-      <div class="deliverType-container__content">
-        <div class="deliverType-container__content__total">Total Price</div>
-        <div class="deliverType-container__content__price">160</div>
-        <div class="deliverType-container__content__currency">EGP</div>
+      <div className="deliverType-container__content">
+        <div className="deliverType-container__content__total">Total Price</div>
+        <div className="deliverType-container__content__price">160</div>
+        <div className="deliverType-container__content__currency">EGP</div>
       </div>
 
-      <div class="recommindationContainer">
-        <div class="recommindationContainer__content">
+      <div className="recommindationContainer">
+        <div className="recommindationContainer__content">
           <div>Recommindations</div>
           <Textarea></Textarea>
         </div>
